91c4dc372e9a4b794e121753632bf3d530c795fe
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-06-03 11:49:33 +0500

    origin

---
+++ 300x600-variant-2/assets/js/index.js
@@ -0,0 +1,39 @@
+let currentSlide = 0;
+
+const slides = document.querySelectorAll(".ad-sub-container");
+const overlay = document.querySelector(".ad-container-overlay");
+
+setInterval(() => {
+  overlay.classList.add("active");
+  let lastSlide = currentSlide;
+  currentSlide = currentSlide != slides.length - 1 ? currentSlide + 1 : 0;
+
+  setTimeout(() => {
+    slides[lastSlide].classList.add("hidden");
+    overlay.classList.add("hidden");
+    slides.forEach((slide, slideIndex) => {
+      if (slideIndex == lastSlide) {
+        const content = slide.querySelector(".ad-content")
+          ? slide.querySelector(".ad-content")
+          : slide.querySelector(".ad-content-last-slide");
+        content.classList.add("hidden");
+        setTimeout(() => {
+          content.classList.remove("hidden");
+        }, 500);
+      }
+      if (slideIndex == currentSlide) {
+        slide.classList.add("animate-it");
+      } else {
+        slide.classList.remove("animate-it");
+      }
+    });
+    setTimeout(() => {
+      overlay.classList.remove("active");
+
+      slides[lastSlide].classList.remove("hidden");
+      setTimeout(() => {
+        overlay.classList.remove("hidden");
+      }, 100);
+    }, 100);
+  }, 900);
+}, 5000);
